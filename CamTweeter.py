015d96caab66d2b8b6fa0c869d547b5886c87a21
Leenix <leenix48@gmail.com>
2014-12-09 14:40:02 +1000

    Included main script
    - Sanitised account-specific details from script
    
    The script is very basic and does not use any exception handling.

---
+++ CamTweeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin.env python
+__author__ = 'Leenix'
+
+from pygame import camera, image
+from twython import Twython
+import logging
+import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+
+def sign_in_to_twitter():
+    CONSUMER_KEY = '<Paste Consumer key (API Key) here>'
+    CONSUMER_SECRET = '<Paste Consumer Secret (API Secret) here>'
+    ACCESS_TOKEN = '<Paste Access Token here>'
+    ACCESS_SECRET = '<Paste Access Token Secret here>'
+
+    return Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+if __name__ == '__main__':
+    # Initialise camera
+    camera.init()
+    camlist = camera.list_cameras()
+    cam = camera.Camera(camlist[0])
+    cam.start()
+
+    # Get an image from the camera
+    image = cam.get_image()
+    logging.info("Image captured")
+    cam.stop()
+
+    # Upload to Twitter
+    now = datetime.datetime.now()
+    time_string = now.strftime("%Y-%m-%d %T")
+    image.save(image, "/tmp/image.jpg")
+    photo = open("/tmp/image.jpg", "rb")
+    twitter = sign_in_to_twitter()
+    twitter.update_status_with_media(status=time_string, media=photo)
